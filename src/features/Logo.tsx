1547a99cc58a98c71b71d7efbeed8096f9a11778
schedular@inc <schedularinc@protonmail.com>
2022-09-18 16:43:03 -0400

    feat: add Hello World banner

---
+++ src/features/Logo.tsx
@@ -0,0 +1,18 @@
+import { floating } from '@/animations/variants';
+import { motion } from 'framer-motion';
+
+const Logo = () => {
+  return (
+    <motion.div
+      className='flex w-full justify-center text-center'
+      variants={floating}
+    >
+      <div className='blur-xl absolute w-[32rem] h-20 mt-5 bg-gradient-to-tr from-pink-700 to-purple-700 opacity-10 rounded-full' />
+      <h1 className='md:text-[5rem] text-[3rem] font-extrabold w-[30rem] text-transparent bg-clip-text bg-gradient-to-tr from-pink-700 to-purple-700'>
+        Hello World
+      </h1>
+    </motion.div>
+  );
+};
+
+export default Logo;
