926eeade05b9996ccd77181fc8e00f56bf95959e
schedular@inc <schedularinc@protonmail.com>
2022-09-18 09:09:42 -0400

    feat(document): add weight to karla font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href='https://fonts.googleapis.com/css2?family=Karla:wght@200;300;400;500&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Karla:wght@200;300;400;500;800&display=swap'
           rel='stylesheet'
         />
       </Head>
